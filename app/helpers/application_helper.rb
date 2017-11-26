abf62418ef13a55115190f0e981aea122ae99dd0
tky3a <takayamac@icloud.com>
2017-11-26 09:52:48 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+###ブラウザ（ページ）のタイトル名表示を設定
+ #page_titleが空の場合base_titleを表示
+ #違う場合はbase_title | page_titleで表示する
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutotial"
+      if page_title.empty? #empty?で配列が空であればtrue、1つ以上の要素があればfalseを返す
+        base_title
+      else
+        "#{base_title} | #{page_title}"
+      end
+  end
 end
