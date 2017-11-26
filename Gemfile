40ac5fe84ab9879fac54af8e538130a4808919d7
tky3a <takayamac@icloud.com>
2017-11-26 11:35:01 +0900

    Add a css definition

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+#bootstrap3
+gem 'bootstrap-sass'
 #jquery
 gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module RailsApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)#Bootstrapを使うための追加
   end
 end
