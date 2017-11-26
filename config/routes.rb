944f987857e5380f4c60a3833722457c5814707b
tky3a <takayamac@icloud.com>
2017-11-26 19:19:01 +0900

    Finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get 'static_pages/help'
+  match '/help', to: 'static_pages#help', via: 'get'
+  #get 'static_pages/about'
+  match '/about', to: 'static_pages#about', via: 'get'
+  #get 'static_pages/contact'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 end
